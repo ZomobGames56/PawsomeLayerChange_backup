b4c0f6803ba0466fdf1c9bc4ef18aa2a164563d2
ZomobGames56 <zomobgames@adstacks.in>
2026-04-21 15:57:28 +0530

    Upadated App

--- Assets/AddOns/HY_Assets/HY_Scripts/TestScript/SplashScreenSceneScript.cs
+++ Assets/AddOns/HY_Assets/HY_Scripts/TestScript/SplashScreenSceneScript.cs
@@ -4,21 +4,14 @@ using UnityEngine.SceneManagement;
 
 public class SplashScreenSceneScript : MonoBehaviour
 {
-    bool once;
     private void Start()
     {
-        once = false;
         StartCoroutine(loadScene());
     }
-    private void Update()
-    {
-        if(!once)
-       
-        once = true;
-    }
+   
     IEnumerator loadScene()
     {
         yield return new WaitForSeconds(2f);
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene(0);
     }
 }
